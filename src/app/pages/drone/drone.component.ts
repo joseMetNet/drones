860fa08f7ade4c2808f03c70aba2665a905fed62
cyara <cmyarap@gmail.com>
2023-04-03 15:59:53 -0500

    CY se realizan ajustes en el calculo de los servicios

--- src/app/pages/drone/drone.component.ts
+++ src/app/pages/drone/drone.component.ts
@@ -10,22 +10,22 @@ import Swal from 'sweetalert2';
   styleUrls: ['./drone.component.scss']
 })
 export class DroneComponent {
-  vehicleForm:FormGroup;
-  customer:any;
-  vehicleStation:any;
-  vehicleStationNumber:number=1;;
-  customerNumber:number=1;;
-  deposit:number=1;
-  showCoordinates:boolean=false;
-  request:any[]=[];
-  isLoading:boolean=false;
+  vehicleForm: FormGroup;
+  customer: any;
+  vehicleStation: any;
+  vehicleStationNumber: number = 1;;
+  customerNumber: number = 1;;
+  deposit: number = 1;
+  showCoordinates: boolean = false;
+  request: any[] = [];
+  isLoading: boolean = false;
   //ENTREGAS DE CLIENTES
-  customerDelivery:any[]=[];
+  customerDelivery: any[] = [];
   //RECOGIDAS DE CLIENTES
-  customerColletion:any[]=[];
+  customerColletion: any[] = [];
   constructor(private formBuilder: FormBuilder,
-    public bsLogicService:BusinessService,
-    private conService:ConnectionService){
+    public bsLogicService: BusinessService,
+    private conService: ConnectionService) {
     this.vehicleForm = this.formBuilder.group({
       deposit: [0, [Validators.required]],
       vehicle_station: [null, [Validators.required]],
@@ -47,11 +47,11 @@ export class DroneComponent {
     });
   }
 
-  get coordinatesArray(){
+  get coordinatesArray() {
     return this.vehicleForm.get('coordinates') as FormArray;
   }
 
-  addCoordinates(){
+  addCoordinates() {
     const coords = (this.vehicleForm.controls['coordinates'] as FormArray);
     coords.push(this.formBuilder.group({
       x: '',
@@ -63,10 +63,10 @@ export class DroneComponent {
     while (formArray.length !== 0) formArray.removeAt(0)
   }
 
-  validateCoordinates(){
-    this.vehicleForm.patchValue({coordinates:[]})
+  validateCoordinates() {
+    this.vehicleForm.patchValue({ coordinates: [] })
     this.clearFormArray(this.coordinatesArray)
-    let rows= this.vehicleStationNumber + this.customerNumber;
+    let rows = this.vehicleStationNumber + this.customerNumber;
     for (let i = 0; i <= rows; i++) this.addCoordinates()
     this.customerDelivery = []
     this.customerColletion = []
@@ -74,13 +74,14 @@ export class DroneComponent {
     this.showCoordinates = true;
   }
 
-  onSubmit(){
+  onSubmit() {
+    this.request = []
     // 1. Delete Processes
     this.conService.deleteProcesses();
     this.isLoading = true;
-    let deposit= '0';
-    let vehicle_station= this.vehicleStation
-    let customer= this.customer
+    let deposit = '0';
+    let vehicle_station = this.vehicleStation
+    let customer = this.customer
     this.vehicleForm.patchValue({
       deposit: deposit,
       /* vehicle_station: vehicle_station,
@@ -88,18 +89,18 @@ export class DroneComponent {
     })
     // 2. Create Station
     let arrayStation = []
-    arrayStation.push({nameTypeStation: 'Deposito',numStation:'0'})
-    this.vehicleStation.map((el:any) =>{
+    arrayStation.push({ nameTypeStation: 'Deposito', numStation: '0' })
+    this.vehicleStation.map((el: any) => {
       let data = { nameTypeStation: 'Vehicles', numStation: el }
       arrayStation.push(data)
     })
-    this.customer.map((el:any) =>{
+    this.customer.map((el: any) => {
       let data = { nameTypeStation: 'Client', numStation: el }
       arrayStation.push(data)
     })
-    arrayStation.map(el =>{
+    arrayStation.map(el => {
       this.conService.createStations(el).subscribe({
-        next:(res:any)=>{
+        next: (res: any) => {
           console.log(res);
         }
       })
@@ -109,82 +110,103 @@ export class DroneComponent {
     this.conService.createRestrictions({
       nameTypeRestriction: 'Capacidad de carga (Q)',
       valueRestriction: this.vehicleForm.controls['loading_capacity_vehicle'].value
-    }).subscribe({next:(res:any)=>{}})
+    }).subscribe({ next: (res: any) => { } })
     this.conService.createRestrictions({
       nameTypeRestriction: 'Capacidad de carga (Q)',
       valueRestriction: this.vehicleForm.controls['loading_capacity_drone'].value
-    }).subscribe({next:(res:any)=>{}})
+    }).subscribe({ next: (res: any) => { } })
     this.conService.createRestrictions({
       nameTypeRestriction: 'Rango de Vuelo',
       valueRestriction: this.vehicleForm.controls['flight_range'].value
-    }).subscribe({next:(res:any)=>{}})
+    }).subscribe({ next: (res: any) => { } })
     // 4. Insert coordinates
-    this.vehicleForm.controls['coordinates'].value.map((el:any, idx:number) =>{
+    this.vehicleForm.controls['coordinates'].value.map((el: any, idx: number) => {
       let data = {
         coordinateX: el.x,
         coordinateY: el.y,
         station: (idx).toString()
       }
       this.conService.insertCoordinates(data).subscribe({
-        next:(el:any)=>{
+        next: (el: any) => {
           console.log(el);
         }
       })
     })
-    // 5.Calculate Distances
-    //this.conService.calculateDistances({stringStation: this.vehicleForm.controls['deposit'].value}).subscribe({next:(res:any)=>{console.log(res);}});
-    this.conService.calculateDistances({stringStation: this.vehicleForm.controls['vehicle_station'].value}).subscribe({next:(res:any)=>{console.log(res);}});
-    //this.conService.calculateDistances({stringStation: this.vehicleForm.controls['customer'].value}).subscribe({next:(res:any)=>{console.log(res);}});
-    // 6. Save Closer Station
-    this.conService.saveCloserStation().subscribe({next:(res:any)=>{console.log(res);
-    }});
-    // 7. Save Closer Saving
-    this.conService.saveCloserSavings().subscribe({next:(res:any)=>{console.log(res);
-    }});
-    // 8. Create Customer Delivery
-    this.conService.createCustomerDelivery().subscribe({
-      next:(res:any)=>{ if (res) this.customerDelivery = res }
-    });
-    // 9. Customer Collection
-    this.conService.customerColletion().subscribe({
-      next:(res:any)=> {if (res) this.customerColletion = res}
-    });
-    // 10. Create Resumen
-    this.conService.createResume().subscribe({next:(res:any)=>{console.log(res);
-    }});
-    // 11. Create Partial Way
-    this.conService.createPartialWay().subscribe({next:(res:any)=>{console.log(res);
-    }});
-    // 12. Create Final Way
-    this.conService.createFinalWay().subscribe(
-      {
-        next:(res:any)=>{
-          if (res) {
-            this.isLoading = false;
-            Swal.fire({
-              title: '¡Perfecto!',
-              text: "Ruta calculada ",
-              icon: 'success',
-              showCancelButton: true,
-              confirmButtonColor: '#28a745',
-              confirmButtonText: 'Ver ruta'
-            }).then((result) => {
-              if (result.value){
-                res.map((el:any)=> this.request.push(el.idStation))
-              }
-            })
-          }
-        },
-        error:(err)=>{
-          this.isLoading = false;
-          Swal.fire({
-            title: 'Error',
-            text: "No fue posible generar una ruta " ,
-            icon: 'error',
-          })
+    setTimeout(() => {
+      // 5.Calculate Distances
+      //this.conService.calculateDistances({stringStation: this.vehicleForm.controls['deposit'].value}).subscribe({next:(res:any)=>{console.log(res);}});
+      this.conService.calculateDistances({ stringStation: this.vehicleForm.controls['vehicle_station'].value })
+      .subscribe({
+        next: (res: any) => { console.log(res); },
+        complete:()=>{
+          // 6. Save Closer Station
+          this.conService.saveCloserStation().subscribe({
+            next: (res: any) => {console.log(res);},
+            complete:()=>{
+              // 7. Save Closer Saving
+              this.conService.saveCloserSavings().subscribe({
+                next: (res: any) => {console.log(res);},
+                complete:()=>{
+                  // 8. Create Customer Delivery
+                  this.conService.createCustomerDelivery().subscribe({
+                    next: (res: any) => { if (res) this.customerDelivery = res },
+                    complete:()=>{
+                      // 9. Customer Collection
+                      this.conService.customerColletion().subscribe({
+                        next: (res: any) => { if (res) this.customerColletion = res },
+                        complete: ()=>{
+                          // 10. Create Resumen
+                          this.conService.createResume().subscribe({
+                            next: (res: any) => {console.log(res);},
+                            complete:()=>{
+                              // 11. Create Partial Way
+                              this.conService.createPartialWay().subscribe({
+                                next: (res: any) => {console.log(res);},
+                                complete:()=>{
+                                  // 12. Create Final Way
+                                  this.conService.createFinalWay().subscribe(
+                                    {
+                                      next: (res: any) => {
+                                        if (res) {
+                                          this.isLoading = false;
+                                          Swal.fire({
+                                            title: '¡Perfecto!',
+                                            text: "Ruta calculada ",
+                                            icon: 'success',
+                                            showCancelButton: true,
+                                            confirmButtonColor: '#28a745',
+                                            confirmButtonText: 'Ver ruta'
+                                          }).then((result) => {
+                                            if (result.value) {
+                                              res.map((el: any) => this.request.push(el.idStation))
+                                            }
+                                          })
+                                        }
+                                      },
+                                      error: (err) => {
+                                        this.isLoading = false;
+                                        Swal.fire({
+                                          title: 'Error',
+                                          text: "No fue posible generar una ruta ",
+                                          icon: 'error',
+                                        })
+                                      }
+                                    }
+                                  );
+                                }
+                              });
+                            }
+                          });
+                        }
+                    });
+                    }
+                });
+                }
+            });
+            }
+          });
         }
-      }
-    );
-    //this.vehicleForm.reset();
+      });
+    }, 1000);
   }
 }
